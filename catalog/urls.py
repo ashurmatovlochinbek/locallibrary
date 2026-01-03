4a6a01efedc0a4ce1289b0291c3a4fd5a504633f
ashurmatovlochinbek <126878017+ashurmatovlochinbek@users.noreply.github.com>
2026-01-03 11:08:53 +0500

    forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('all-borrowed/', views.AllBorrowedBooksListView.as_view(), name='all-borrowed')
+    path('all-borrowed/', views.AllBorrowedBooksListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/renew/', views.RenewBookLibrarianView.as_view(), name='renew-book-librarian'),
 ]
 
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
